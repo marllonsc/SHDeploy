4a08f36a0e96c4e5ee0765365032c59b2b0bb7cd
Marllon Costa <marllonsc@gmail.com>
2023-12-12 09:43:57 -0300

    add the command npm i

--- src/main/java/com/marllonsc/br/util/FileActions.java
+++ src/main/java/com/marllonsc/br/util/FileActions.java
@@ -123,6 +123,7 @@ public class FileActions {
 
             return "cd "+project.getPathApp()+ " \n"
                     + "git pull \n" 
+                     + "npm i \n" 
                     + "npm run build \n" 
                     + "docker build -t " + project.getName() + " . \n"
                     + "docker run -d -p "+project.getIpPort()+":80 --name "+project.getName() +" "+ project.getName() + ":latest "  
