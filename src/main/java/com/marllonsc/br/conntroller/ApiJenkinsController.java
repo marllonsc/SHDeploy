bbad1e28b6b63b7626c0b5469a56aaf107dc415e
marllon <marllon@ibm.com>
2024-06-08 07:36:58 -0300

    add endpoint to jenkins

--- src/main/java/com/marllonsc/br/conntroller/ApiJenkinsController.java
+++ src/main/java/com/marllonsc/br/conntroller/ApiJenkinsController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 public class ApiJenkinsController {
 
     private final ProjectService projectService;
